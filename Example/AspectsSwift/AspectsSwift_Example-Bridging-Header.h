0ccefd8320d222e82e81d3b2fed9430e6f4c8220
chenchangqing <chenchangqing198@126.com>
2016-01-01 23:05:25 +0800

    temp ci

---
+++ Example/AspectsSwift/AspectsSwift_Example-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AspectsSwift-Example-Bridging-Header.h
+//  AspectsSwift
+//
+//  Created by green on 16/1/1.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+#ifndef AspectsSwift_Example_Bridging_Header_h
+#define AspectsSwift_Example_Bridging_Header_h
+
+
+#endif /* AspectsSwift_Example_Bridging_Header_h */
